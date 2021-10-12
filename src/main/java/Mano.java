903210b7a172823369cf2821edeaadd585190213
jonathvilla <76117705+jonathvilla@users.noreply.github.com>
2021-10-11 22:36:22 -0500

    feat: Se implementa la clase mano

---
+++ src/main/java/Mano.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class Mano {
+
+	
+	private List<Carta>mano;
+
+	public List<Carta> getMano() {
+		return mano;
+	}
+
+	public void setMano(List<Carta> mano) {
+		this.mano = mano;
+	}
+	
+	
+}
