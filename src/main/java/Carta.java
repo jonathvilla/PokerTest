d7f9d351604081ea9b2b3a1a7ed8fc9e5139a390
jonathvilla <76117705+jonathvilla@users.noreply.github.com>
2021-10-11 22:17:24 -0500

    fear:se crea clase carta

--- src/main/java/Carta.java
+++ src/main/java/Carta.java
@@ -1,4 +1,46 @@
 
-public class Carta {
+public class Carta implements Comparable<Carta> {
+	
+	private valorEnum valor;
+	private paloEnum palo;
+	
+	
+	public Carta(valorEnum valor, paloEnum palo) {
+		
+		this.valor = valor;
+		this.palo = palo;
+	}
 
+
+	public valorEnum getValor() {
+		return valor;
+	}
+
+	public void setValor(valorEnum valor) {
+		this.valor = valor;
+	}
+
+
+	public paloEnum getPalo() {
+		return palo;
+	}
+
+
+	public void setPalo(paloEnum palo) {
+		this.palo = palo;
+	}
+	@Override
+	public String toString() {
+		return "Carta [valor=" + valor + ", palo=" + palo + ", getValor()=" + getValor() + ", getPalo()=" + getPalo()
+				+ "]";
+	}
+
+
+	public int compareTo(Carta o) {
+		int comparar = this.valor.compareTo( o.valor);
+		if(comparar!=0) {
+			return comparar;
+		}
+		return 0;
+	}
 }
