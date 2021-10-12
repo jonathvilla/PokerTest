f6bf2193270b3843d2401e508d0a241509a9fb68
jonathvilla <76117705+jonathvilla@users.noreply.github.com>
2021-10-11 22:19:42 -0500

    feat: Se implementa las clase valorEnum, paloEnum

--- src/main/java/paloEnum.java
+++ src/main/java/paloEnum.java
@@ -1,4 +1,24 @@
 
 public enum paloEnum {
 
+	CORAZONES("C"), TREBOL("H"), DIAMANTES("D"), ESPADAS("S");
+	
+	private String palo;
+	
+	private paloEnum(String palo) {
+		this.palo = palo;
+		
+	}
+
+	public String getPalo() {
+		return palo;
+	}
+
+	public void setPalo(String palo) {
+		this.palo = palo;
+	}
+	
+	
+	
+	
 }
--- src/main/java/valorEnum.java
+++ src/main/java/valorEnum.java
@@ -1,4 +1,15 @@
 
 public enum valorEnum {
+	
+	DOS(2), TRES(3),CUATRO(4),CINCO(5), SEIS(6),SIETE(7),OCHO(8),NUEVE(9),DIEZ(10), J(11), Q(12), K(13), AS(14);
 
+	
+	private int valorNumero;
+	
+	private valorEnum(int valor) {
+		this.valorNumero= valor;
+	}
+	public int getValorNumero() {
+		return this.valorNumero;
+	}
 }
